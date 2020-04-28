a9ad3eed2a92c0a710713786586606a7625a1128
zhuxiaofeng1992 <37865021+zhuxiaofeng1992@users.noreply.github.com>
2020-04-28 14:56:35 +0800

    Add files via upload

---
+++ CommonLibrary/Enums/Enum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Enums
+{
+    public enum ErrorMsgEnum
+    {
+        CannotConnectToOPCServer = 0,
+    }
+    public enum WarningMsgEnum
+    {
+
+    }
+}
